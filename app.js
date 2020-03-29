6ff04044adbe0965e2c29f42e76e20084bb6914a
Lillian Gales <lilliangales@ymail.com>
2020-03-28 21:17:23 -0700

    added on touch start for mobile view

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 'use strict';
 
 document.querySelector("#demo-petster-button").onclick = function (event) {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  console.log(w, h)
   document.querySelector(".pop-up-demo").style.display = "flex";
   document.querySelector("#content").style.opacity = ".5";
 }
 
 document.querySelector(".fas.fa-times").onclick = function (event) {
+  console.log('hik')
   document.querySelector(".pop-up-demo").style.display = "none";
   document.querySelector("#content").style.opacity = "1";
+}
+
+document.querySelector(".project-card").ontouchstart = function (event) {
+  if (window.innerWidth < 500) {
+    document.querySelector(".project-card-inner").style.transform = "rotateY(180 deg)";
+  }
 }
