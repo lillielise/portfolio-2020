78609d5f36d43224adde6d5207b7e0e46fdfbb14
Lillian Gales <lilliangales@ymail.com>
2020-03-28 21:27:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,3 @@ document.querySelector(".fas.fa-times").onclick = function (event) {
   document.querySelector(".pop-up-demo").style.display = "none";
   document.querySelector("#content").style.opacity = "1";
 }
-
-document.querySelector(".project-card").ontouchstart = function (event) {
-  if (window.innerWidth < 500) {
-    document.querySelector(".project-card-inner").style.transform = "rotateY(180 deg)";
-  }
-}
