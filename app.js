b180147d4cd2bc755a4a007e93f1c8b21dddb019
Lillian Gales <lilliangales@ymail.com>
2020-03-31 00:58:58 -0700

    added function to handle demo click

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict";
 
-document.querySelector("#demo-petster-button").onclick = function(event) {
-  document.querySelector(".pop-up-demo").style.display = "flex";
-  document.querySelector("#content").style.opacity = ".8";
-};
-
 document.querySelector(".fas.fa-times").onclick = function(event) {
-  console.log("hik");
   document.querySelector(".pop-up-demo").style.display = "none";
   document.querySelector("#content").style.opacity = "1";
 };
+
+function handleDemoClick() {
+  console.log("heee");
+  document.querySelector(".pop-up-demo").style.display = "flex";
+  document.querySelector("#content").style.opacity = ".8";
+}
