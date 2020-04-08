d052ccbc077ee6055be2c5240061cc88e4a025e2
Lillian Gales <lilliangales@ymail.com>
2020-04-07 17:24:01 -0700

    added mobile description on back

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ document.querySelector(".fas.fa-times").onclick = function (event) {
   document.querySelector("#content").style.opacity = "1";
 };
 
-document.querySelector(".project-card-front").onclick = function (event) {
-  document.querySelector(".project-card-inner").style.transform =
-    "rotateY(180deg)";
-};
-
-document.querySelector(".project-card-back").onclick = function (event) {
-  document.querySelector(".project-card-inner").style.transform =
-    "rotateY(360deg)";
-};
-
 function handleDemoClick() {
   document.querySelector(".pop-up-demo").style.display = "flex";
   document.querySelector("#content").style.opacity = ".8";
